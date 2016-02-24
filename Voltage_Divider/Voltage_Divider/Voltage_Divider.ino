83d90f2d23531484fb7f7e2960661b8059032845
Richard Opoku <anabeto93@gmail.com>
2016-02-24 01:05:49 +0000

    voltage divider law to sense input voltage
    Relay on R1=6K and R2=1K from an AC voltage of about 12V rms

---
+++ Voltage_Divider/Voltage_Divider/Voltage_Divider.ino
@@ -0,0 +1,29 @@
+int sum=0;
+int sensorPin=A2;
+float voltage = 0.0;
+float sample =0;
+
+void setup()
+{
+  Serial.begin(9600);  
+}
+
+void loop()
+{
+  for(int i=0; i<150; ++i)
+  {
+    
+    sample+= analogRead(sensorPin);
+    delay(2);//delay by a 2 miliseconds 
+  }
+  //after sampling, find the average
+  sample /=150;
+  voltage = (5.45*7*sample)/1000;
+  Serial.print("ADC = ");
+  Serial.print(sample);
+  Serial.print("\t Voltage =");
+  Serial.print(voltage);
+  Serial.println(" V");
+  /*Serial.println(analogRead(sensorPin));
+  delay(2000);//delay by 2 seconds*/
+}
