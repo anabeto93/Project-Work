b10fde3a645e86b250b6af3f7baf724b6a6fca98
Richard Opoku <anabeto93@gmail.com>
2016-02-24 01:01:41 +0000

    5V Power supply for the Arduino Yun
    Use the 7805 to regulate 5V to power the arduino after stepping it down using a 230||12V transformer
    
    Seems risky but works

---
+++ Voltage_Regulator/Voltage_Regulator/Voltage_Regulator.ino
@@ -0,0 +1,26 @@
+int voltPin = A2;
+int ledPin =5;
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(voltPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(voltPin);
+  Serial.println(value);
+  delay(500);
+  float val = (float)((value/1023.0)*5);
+  Serial.print(val);
+  Serial.println(" V");
+  delay(500);
+  /*if(voltPin>=4)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  delay(3000);
+  digitalWrite(ledPin,LOW);
+  delay(3000);*/
+}
